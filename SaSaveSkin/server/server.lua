3606bfc537f5daf8c96740ef4d2598e1fe998461
ImSamaCurrent <118637845+ImSamaCurrent@users.noreply.github.com>
2024-02-21 10:02:39 +0100

    Update server.lua

--- SaSaveSkin/server/server.lua
+++ SaSaveSkin/server/server.lua
@@ -2,9 +2,9 @@
 ESX = exports['es_extended']:getSharedObject()
 
 admins = {
-    'license:88134ec7ca7575ffdd64be6bfb1dd94581bc56db',
-    'license:274b6a1992286eedd0b5277662d57887300cfb93',
-    'license:0ad91cae300dd74524176c616fd6df5c9783f133'
+    'license:XXX',
+    'license:XXX',
+    'license:XXX'
 }
 
 
